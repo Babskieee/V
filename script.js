89058babb144ca36a4e898e9db7d6743ffdfccf3
Babskie <ronilosepida13@gmail.com>
2023-02-14 11:15:07 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("HI!");
+alert("I know that you have plans today");
+alert("And I also can't go out because of health problem");
+alert("So here's my little gift for you =)");
+alert("Happy Valentine's Day!");
